46ba1ff6169a0cc21e363c2ba41aaee1eb3d1c3d
suriya001 <suriya001.sj@gmail.com>
2017-03-15 21:30:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('article-one', function (req,res) {
-    res.send("article one is requested and it will b served here")
+    res.send("article one is requested and it will b served here");
 });
 
 app.get('article-two', function (req,res) {
-    res.send("article two is requested and it will b served here")
+    res.send("article two is requested and it will b served here");
 });
 
-app.get('article-two', function (req,res) {
-    res.send("article three is requested and it will b served here")
+app.get('article-three', function (req,res) {
+    res.send("article three is requested and it will b served here");
 });
 
 app.get('/ui/style.css', function (req, res) {
